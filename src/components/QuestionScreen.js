49640245f270bfdfff011ba10d8771338ea3dd0f
OmarD <omar.daghestani@colorado.edu>
2024-02-25 23:24:00 -0700

    cleaned console logs

--- src/components/QuestionScreen.js
+++ src/components/QuestionScreen.js
@@ -12,7 +12,6 @@ function QuestionScreen({ topic }) {
 
   // Timer countdown logic
   useEffect(() => {
-    console.log(`Timer: ${timer}, Selected Answer: ${selectedAnswer}`);
     if (timer > 0 && !selectedAnswer) {
       const intervalId = setInterval(() => {
         setTimer((prevTimer) => prevTimer - 1);
@@ -30,38 +29,27 @@ function QuestionScreen({ topic }) {
   // Handle scoring and setting trigger for the next question
   useEffect(() => {
     if (selectedAnswer && !hasScoreUpdated) {
-      console.log(`Answer selected: ${selectedAnswer}`);
       if (selectedAnswer === questions[currentQuestionIndex].answer) {
         setScore((prevScore) => prevScore + timer * 100);
-        console.log(`Score updated to: ${score + timer * 100}`);
       }
-  
-      console.log('Setting timeout to move to next question...');
+
       const timeoutId = setTimeout(() => {
-        console.log('Timeout completed, setting trigger for next question...');
         setTriggerNextQuestion(true);
         setHasScoreUpdated(true); // Update the flag here after the timeout
       }, 1000);
-  
-      return () => {
-        console.log('Clearing timeout...');
-        clearTimeout(timeoutId);
-      };
+
+      return () => clearTimeout(timeoutId);
     }
   }, [selectedAnswer, hasScoreUpdated, currentQuestionIndex, questions, timer]);
   
-  
   // Handle the transition to the next question
   useEffect(() => {
-    console.log('Effect for triggering next question', triggerNextQuestion);
     if (triggerNextQuestion) {
-      console.log('Trigger received, calling handleNextQuestion...');
       handleNextQuestion();
     }
   }, [triggerNextQuestion]);
 
   const handleNextQuestion = () => {
-    console.log('handleNextQuestion called');
     if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
       setTimer(10);
@@ -69,7 +57,6 @@ function QuestionScreen({ topic }) {
       setHasScoreUpdated(false);
       setTriggerNextQuestion(false);  // Resetting the trigger here.
     } else {
-      console.log('Quiz finished, showing score...');
       alert(`Quiz finished! Your score: ${score}`);
     }
   };
